ca918e10f5650dc2b379ec81ab464bc90aca58a6
cassiadev <37288667+cassiadev@users.noreply.github.com>
2020-10-24 08:22:40 +0900

    Added test case of getting data by synchronization
    Using runBlocking and await

--- app/src/test/java/com/mmp/mymapdiary/ExampleUnitTest.kt
+++ app/src/test/java/com/mmp/mymapdiary/ExampleUnitTest.kt
@@ -1,5 +1,10 @@
 package com.mmp.mymapdiary
 
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +18,42 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun checkMapsType() {
+        assertTrue(getMaps() is List<SelectAllMapsQuery.Map?>?)
+    }
+
+    private fun getMaps() = runBlocking {
+
+        // Create an ApolloClient with the project's endpoint
+        val apolloClient = ApolloClient.builder()
+            .serverUrl("https://5ahxbvmodj.execute-api.us-east-1.amazonaws.com/dev/graphql")
+            .build()
+
+        // Obtain response by synchronization in the coroutine scope
+        val request = async<List<SelectAllMapsQuery.Map?>?> {
+            val response = try {
+                apolloClient.query(SelectAllMapsQuery()).toDeferred().await()
+            } catch (e: ApolloException) {
+                // handle protocol errors
+                null!!
+            }
+
+            val maps = response.data?.maps
+            if (maps == null || response.hasErrors()) {
+                // handle applicationo errors
+                null!!
+            }
+
+            // The launched now contains type-safe model of the data
+            println("maps: ${maps}")
+            maps
+        }
+
+        val result = request.await()
+        println("getMaps: ${result}")
+        result
     }
 }
